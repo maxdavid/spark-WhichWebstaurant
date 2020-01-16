668fe5554857da3507ea93e2d35595e89b98787f
Max David <me@maxdavid.com>
2020-01-16 11:21:48 -0800

    Add item inventory, text info inventory

---
+++ inventory/catalog.js
@@ -0,0 +1,248 @@
+const catalog = [
+  {
+    id: 000,
+    title: "Acopa Dual-Sided Flight Paddle with Square Tasting Bowls - 3/Case",
+    code: "Item #: 303BWSQDUAL",
+    price: "$42.59"
+  },
+  {
+    id: 001,
+    title: "Garland SS686 Sentry Series 6 Sealed Burner Electric Restaurant Range with Standard Oven - 240V, 1 Phase, 19 kW",
+    code: "Item #: 372SS686C",
+    price: "$6,483.00"
+  },
+  {
+    id: 002,
+    title: 'Regency 16 Gauge Stainless Steel 18" x 48" Heavy Duty Wall Shelf',
+    code: "Item #: 600WS1848HD",
+    price: "$170.92"
+  },
+  {
+    id: 003,
+    title: "Carlisle TTS10003 Black Standard Duty 1 Cubic Yard Utility Tilt Truck / Trash Cart",
+    code: "Item #: 271TTS10003",
+    price: "$497.99"
+  },
+  {
+    id: 004,
+    title: "Choice Deluxe 8 Qt. Full Size Roll Top Chafer",
+    code: "Item #: 407COH4",
+    price: "$66.79"
+  },
+  {
+    id: 005,
+    title: "CaterGator 10 Gallon Black Insulated Beverage Dispenser",
+    code: "Item #: 215BEVDP10BK",
+    price: "$143.41"
+  },
+  {
+    id: 006,
+    title: "Noble Warewashing DG Low Temperature Single Rack Glass Washer / Dishwasher",
+    code: "Item #: 495NOWFDGX",
+    price: "$3,159.00"
+  },
+  {
+    id: 007,
+    title: "Solwave 1200W Stackable Commercial Microwave with Large 1.2 cu. ft. Interior and Push Button Controls - 120V",
+    code: "Item #: 180MW1200T",
+    price: "$549.00"
+  },
+  {
+    id: 008,
+    title: 'Metro PCD11A Adjustable "Poker Chip" Dish Dolly',
+    code: "Item #: 461PCD11A",
+    price: "$689.00"
+  },
+  {
+    id: 009,
+    title: "Lancaster Table & Seating Almond Contoured Blow Molded Folding Chair with Charcoal Frame",
+    code: "Item #: 384YCD50ALM",
+    price: "$14.73"
+  },
+  {
+    id: 010,
+    title: 'Choice 14" Non-Stick Aluminum Fry Pan',
+    code: "Item #: 407FRYPANS14",
+    price: "$16.43"
+  },
+  {
+    id: 011,
+    title: "Excel XL-SB XLERATOR® Stainless Steel Cover High Speed Hand Dryer - 208/277V, 1500W",
+    code: "Item #: 136XLSBQ",
+    price: "$676.50"
+  },
+  {
+    id: 012,
+    title: 'Fold-Pak 08BPEARTHM Bio-Plus Kraft Microwavable Paper #8 Take-Out Container - 300/Case',
+    code: "Item #: 79508BPKRAFT",
+    price: "$57.42"
+  },
+  {
+    id: 013,
+    title: 'Avantco GDS-33-HCW 40" White Sliding Glass Door Merchandiser Refrigerator with LED Lighting',
+    code: "Item #: 178GDS33HCW",
+    price: "$1,639.00"
+  },
+  {
+    id: 014,
+    title: "Cooking Performance Group F302 30 lb. Dual Tank Heavy-Duty Electric Countertop Fryer - 208/240V",
+    code: "Item #: 351EF302",
+    price: "$1,199.00"
+  },
+  {
+    id: 015,
+    title: "Grindmaster 1011-009 Mira Series Traditional One Group Espresso Machine - 120V",
+    code: "Item #: 385MIRA",
+    price: "$4,816.95"
+  },
+  {
+    id: 016,
+    title: "Lancaster Table & Seating Black Double Ring Barstool with 3 1/2" Thick Seat",
+    code: "Item #: 164GS605BBLK",
+    price: "$32.99"
+  },
+  {
+    id: 017,
+    title: "Choice 6-Compartment Condiment Bar",
+    code: "Item #: 176CON6XPNT",
+    price: "$10.90"
+  },
+  {
+    id: 018,
+    title: 'Regency 1 Bowl Underbar Hand Sink with Swivel Faucet - 14 1/2" x 18 3/4"',
+    code: "Item #: 600B11014",
+    price: "$202.46"
+  },
+  {
+    id: 019,
+    title: "Vollrath 68391 Wear-Ever 42 Qt. Heavy Duty Aluminum Roasting Pan with Handles",
+    code: "Item #: 92268391 ",
+    price: "$157.99"
+  },
+  {
+    id: 020,
+    title: "Crown Verity CV2650-SV Silver Portable Propane Outdoor Patio Heater - 45,000 BTU",
+    code: "Item #: 255CV2650SL",
+    price: "$419.75"
+  },
+  {
+    id: 021,
+    title: '3" x 12 1/2" Bartender Practice Flair Bottle',
+    code: "Item #: 407PFBT11W",
+    price: "$8.99"
+  },
+  {
+    id: 022,
+    title: "Rubbermaid FG409100GRAY Xtra Gray 300 lb. Three Shelf Utility Cart / Bus Cart",
+    code: "Item #: 690409100GY",
+    price: "$262.00"
+  },
+  {
+    id: 023,
+    title: 'Lavex Janitorial 8" Natural Kraft Hardwound Paper Towel, 800 Feet',
+    code: "Item #: 5001RT800N",
+    price: "$18.29"
+  },
+  {
+    id: 024,
+    title: "Cal-Mil 3449-3-83 Ashwood Gray Oak 3-Tier Vertical Merchandiser",
+    code: "Item #: 2113449383 ",
+    price: "$359.99"
+  },
+  {
+    id: 025,
+    title: "Globe SP60 Gear Driven 60 Qt. Commercial Planetary Floor Mixer - 220V",
+    code: "Item #: 377SP60Z",
+    price: "$11,556.00"
+  },
+  {
+    id: 026,
+    title: 'Avantco SL312 12" Manual Gravity Feed Meat Slicer',
+    code: "Item #: 177SL312",
+    price: "$479.99"
+  },
+  {
+    id: 027,
+    title: 'Cooking Performance Group CBR24 24" Gas Countertop Radiant Charbroiler',
+    code: "Item #: 351CRCPG24NL",
+    price: "$489.99"
+  },
+  {
+    id: 028,
+    title: "Mercer Culinary M18290 Hell's Handle High Heat Heavy-Duty Square Edge Turner",
+    code: "Item #: 470M18290",
+    price: "$21.94"
+  },
+  {
+    id: 029,
+    title: 'Vigor Full Size 6" Deep Anti-Jam Stainless Steel Steam Table / Hotel Pan',
+    code: "Item #: 24711622",
+    price: "$28.12"
+  },
+  {
+    id: 030,
+    title: "Regal Foods Worcestershire Sauce 1 Gallon Bulk Container - Garber's Brand",
+    code: "Item #: 12558333",
+    price: "$27.62"
+  },
+  {
+    id: 031,
+    title: "Ken's Foods Essentials 1 Gallon Creamy Caesar Dressing",
+    code: "Item #: 999991425",
+    price: "$16.99"
+  },
+  {
+    id: 032,
+    title: 'Endless Summer 30" Square LP Gas Outdoor Fire Pit Table with Wood Grain Top',
+    code: "Item #: 472GAD1450",
+    price: "$279.99"
+  },
+  {
+    id: 033,
+    title: "Canola Oil - 35 lb.",
+    code: "Item #: 101CANOLABLK",
+    price: "$18.60"
+  },
+  {
+    id: 034,
+    title: "Regal #10 Can Sliced Black Olives - 6/Case",
+    code: "Item #: 107685209",
+    price: "$27.39"
+  },
+  {
+    id: 035,
+    title: 'Scotsman UN324A-1 24" Air Cooled Undercounter Nugget Ice Machine - 340 lb.',
+    code: "Item #: 720UN324A1 ",
+    price: "$3,543.00"
+  },
+  {
+    id: 036,
+    title: "Bunn VPR Black 12 Cup Pourover Coffee Brewer with 1 Upper and 1 Lower Warmer - 120V",
+    code: "Item #: 234332000015",
+    price: "$271.00"
+  },
+  {
+    id: 037,
+    title: 'Beverage-Air SPED36HC-08-2 36" 2 Drawer Refrigerated Sandwich Prep Table',
+    code: "Item #: 185SED3608H",
+    price: "$1,549.00"
+  },
+  {
+    id: 038,
+    title: "Spaceman 6235AH Soft Serve Ice Cream Machine with Air Pump and 2 Hoppers - 208/230V",
+    code: "Item #: 7156235A",
+    price: "$12,604.00"
+  },
+  {
+    id: 039,
+    title: "Continental Laundry Cart, Huskee 275 Black X Frame Folding Cart",
+    code: "Item #: 274275BK ",
+    price: "$128.58"
+  },
+  {
+    id: 040,
+    title: "Avantco IC3500 Countertop Induction Range / Cooker - 208/240V, 3500W",
+    code: "Item #: 177IC3500",
+    price: "$174.72"
+  },
+]
