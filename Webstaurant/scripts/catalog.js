08f452cec4f334ff96c9aaec084156f7f09b2cd7
Max David <me@maxdavid.com>
2021-03-10 10:35:19 -0800

    Completely rewrite scripts to work with new Spark version

--- Webstaurant/scripts/catalog.js
+++ Webstaurant/scripts/catalog.js
@@ -1,339 +1,664 @@
+const Scene = require('Scene');
+
 export const catalog = [
   {
     id: 0,
     title: 'Acopa Dual-Sided Flight Paddle with Square Tasting Bowls - 3/Case',
     code: 'Item #: 303BWSQDUAL',
-    price: '$42.59'
+    price: '$42.59',
   },
   {
     id: 1,
     title:
       'Garland SS686 Sentry Series 6 Sealed Burner Electric Restaurant Range with Standard Oven - 240V, 1 Phase, 19 kW',
     code: 'Item #: 372SS686C',
-    price: '$6,483.'
+    price: '$6,483.',
   },
   {
     id: 2,
     title: 'Regency 16 Gauge Stainless Steel 18" x 48" Heavy Duty Wall Shelf',
     code: 'Item #: 6WS1848HD',
-    price: '$170.92'
+    price: '$170.92',
   },
   {
     id: 3,
     title:
       'Carlisle TTS103 Black Standard Duty 1 Cubic Yard Utility Tilt Truck / Trash Cart',
     code: 'Item #: 271TTS103',
-    price: '$497.99'
+    price: '$497.99',
   },
   {
     id: 4,
     title: 'Choice Deluxe 8 Qt. Full Size Roll Top Chafer',
     code: 'Item #: 407COH4',
-    price: '$66.79'
+    price: '$66.79',
   },
   {
     id: 5,
     title: 'CaterGator 10 Gallon Black Insulated Beverage Dispenser',
     code: 'Item #: 215BEVDP10BK',
-    price: '$143.41'
+    price: '$143.41',
   },
   {
     id: 6,
     title:
       'Noble Warewashing DG Low Temperature Single Rack Glass Washer / Dishwasher',
     code: 'Item #: 495NOWFDGX',
-    price: '$3,159.'
+    price: '$3,159.',
   },
   {
     id: 7,
     title:
       'Solwave 12W Stackable Commercial Microwave with Large 1.2 cu. ft. Interior and Push Button Controls - 120V',
     code: 'Item #: 180MW12T',
-    price: '$549.'
+    price: '$549.',
   },
   {
     id: 8,
     title: 'Metro PCD11A Adjustable "Poker Chip" Dish Dolly',
     code: 'Item #: 461PCD11A',
-    price: '$689.'
+    price: '$689.',
   },
   {
     id: 9,
     title:
       'Lancaster Table & Seating Almond Contoured Blow Molded Folding Chair with Charcoal Frame',
     code: 'Item #: 384YCD50ALM',
-    price: '$14.73'
+    price: '$14.73',
   },
   {
     id: 10,
     title: 'Choice 14" Non-Stick Aluminum Fry Pan',
     code: 'Item #: 407FRYPANS14',
-    price: '$16.43'
+    price: '$16.43',
   },
   {
     id: 11,
     title:
       'Excel XL-SB XLERATOR® Stainless Steel Cover High Speed Hand Dryer - 208/277V, 15W',
     code: 'Item #: 136XLSBQ',
-    price: '$676.50'
+    price: '$676.50',
   },
   {
     id: 12,
     title:
       'Fold-Pak8BPEARTHM Bio-Plus Kraft Microwavable Paper #8 Take-Out Container - 3/Case',
     code: 'Item #: 79508BPKRAFT',
-    price: '$57.42'
+    price: '$57.42',
   },
   {
     id: 13,
     title:
       'Avantco GDS-33-HCW 40" White Sliding Glass Door Merchandiser Refrigerator with LED Lighting',
     code: 'Item #: 178GDS33HCW',
-    price: '$1,639.'
+    price: '$1,639.',
   },
   {
     id: 14,
     title:
       'Cooking Performance Group F302 30 lb. Dual Tank Heavy-Duty Electric Countertop Fryer - 208/240V',
     code: 'Item #: 351EF302',
-    price: '$1,199.'
+    price: '$1,199.',
   },
   {
     id: 15,
     title:
       'Grindmaster 1011-9 Mira Series Traditional One Group Espresso Machine - 120V',
     code: 'Item #: 385MIRA',
-    price: '$4,816.95'
+    price: '$4,816.95',
   },
   {
     id: 16,
     title:
       'Lancaster Table & Seating Black Double Ring Barstool with 3 1/2" Thick Seat',
     code: 'Item #: 164GS605BBLK',
-    price: '$32.99'
+    price: '$32.99',
   },
   {
     id: 17,
     title: 'Choice 6-Compartment Condiment Bar',
     code: 'Item #: 176CON6XPNT',
-    price: '$10.90'
+    price: '$10.90',
   },
   {
     id: 18,
     title:
       'Regency 1 Bowl Underbar Hand Sink with Swivel Faucet - 14 1/2" x 18 3/4"',
     code: 'Item #: 6B11014',
-    price: '$202.46'
+    price: '$202.46',
   },
   {
     id: 19,
     title:
       'Vollrath 68391 Wear-Ever 42 Qt. Heavy Duty Aluminum Roasting Pan with Handles',
     code: 'Item #: 92268391 ',
-    price: '$157.99'
+    price: '$157.99',
   },
   {
     id: 20,
     title:
       'Crown Verity CV2650-SV Silver Portable Propane Outdoor Patio Heater - 45,0 BTU',
     code: 'Item #: 255CV2650SL',
-    price: '$419.75'
+    price: '$419.75',
   },
   {
     id: 21,
     title: '3" x 12 1/2" Bartender Practice Flair Bottle',
     code: 'Item #: 407PFBT11W',
-    price: '$8.99'
+    price: '$8.99',
   },
   {
     id: 22,
     title:
       'Rubbermaid FG4091GRAY Xtra Gray 3 lb. Three Shelf Utility Cart / Bus Cart',
     code: 'Item #: 6904091GY',
-    price: '$262.'
+    price: '$262.',
   },
   {
     id: 23,
     title: 'Lavex Janitorial 8" Natural Kraft Hardwound Paper Towel, 8 Feet',
     code: 'Item #: 51RT8N',
-    price: '$18.29'
+    price: '$18.29',
   },
   {
     id: 24,
     title: 'Cal-Mil 3449-3-83 Ashwood Gray Oak 3-Tier Vertical Merchandiser',
     code: 'Item #: 2113449383 ',
-    price: '$359.99'
+    price: '$359.99',
   },
   {
     id: 25,
     title:
       'Globe SP60 Gear Driven 60 Qt. Commercial Planetary Floor Mixer - 220V',
     code: 'Item #: 377SP60Z',
-    price: '$11,556.'
+    price: '$11,556.',
   },
   {
     id: 26,
     title: 'Avantco SL312 12" Manual Gravity Feed Meat Slicer',
     code: 'Item #: 177SL312',
-    price: '$479.99'
+    price: '$479.99',
   },
   {
     id: 27,
     title:
       'Cooking Performance Group CBR24 24" Gas Countertop Radiant Charbroiler',
     code: 'Item #: 351CRCPG24NL',
-    price: '$489.99'
+    price: '$489.99',
   },
   {
     id: 28,
     title:
       "Mercer Culinary M18290 Hell's Handle High Heat Heavy-Duty Square Edge Turner",
     code: 'Item #: 470M18290',
-    price: '$21.94'
+    price: '$21.94',
   },
   {
     id: 29,
     title:
       'Vigor Full Size 6" Deep Anti-Jam Stainless Steel Steam Table / Hotel Pan',
     code: 'Item #: 24711622',
-    price: '$28.12'
+    price: '$28.12',
   },
   {
     id: 30,
     title:
       "Regal Foods Worcestershire Sauce 1 Gallon Bulk Container - Garber's Brand",
     code: 'Item #: 12558333',
-    price: '$27.62'
+    price: '$27.62',
   },
   {
     id: 31,
     title: "Ken's Foods Essentials 1 Gallon Creamy Caesar Dressing",
     code: 'Item #: 999991425',
-    price: '$16.99'
+    price: '$16.99',
   },
   {
     id: 32,
     title:
       'Endless Summer 30" Square LP Gas Outdoor Fire Pit Table with Wood Grain Top',
     code: 'Item #: 472GAD1450',
-    price: '$279.99'
+    price: '$279.99',
   },
   {
     id: 33,
     title: 'Canola Oil - 35 lb.',
     code: 'Item #: 101CANOLABLK',
-    price: '$18.60'
+    price: '$18.60',
   },
   {
     id: 34,
     title: 'Regal #10 Can Sliced Black Olives - 6/Case',
     code: 'Item #: 107685209',
-    price: '$27.39'
+    price: '$27.39',
   },
   {
     id: 35,
     title:
       'Scotsman UN324A-1 24" Air Cooled Undercounter Nugget Ice Machine - 340 lb.',
     code: 'Item #: 720UN324A1 ',
-    price: '$3,543.'
+    price: '$3,543.',
   },
   {
     id: 36,
     title:
       'Bunn VPR Black 12 Cup Pourover Coffee Brewer with 1 Upper and 1 Lower Warmer - 120V',
     code: 'Item #: 23433215',
-    price: '$271.'
+    price: '$271.',
   },
   {
     id: 37,
     title:
       'Beverage-Air SPED36HC-08-2 36" 2 Drawer Refrigerated Sandwich Prep Table',
     code: 'Item #: 185SED3608H',
-    price: '$1,549.'
+    price: '$1,549.',
   },
   {
     id: 38,
     title:
       'Spaceman 6235AH Soft Serve Ice Cream Machine with Air Pump and 2 Hoppers - 208/230V',
     code: 'Item #: 7156235A',
-    price: '$12,604.'
+    price: '$12,604.',
   },
   {
     id: 39,
     title: 'Continental Laundry Cart, Huskee 275 Black X Frame Folding Cart',
     code: 'Item #: 274275BK ',
-    price: '$128.58'
+    price: '$128.58',
   },
   {
     id: 40,
     title: 'Avantco IC35 Countertop Induction Range / Cooker - 208/240V, 35W',
     code: 'Item #: 177IC35',
-    price: '$174.72'
+    price: '$174.72',
   },
   {
     id: 41,
     title:
       "Regal Foods Worcestershire Sauce 1 Gallon Bulk Container - Garber's Brand",
     code: 'Item #: 12558333',
-    price: '$27.62'
+    price: '$27.62',
   },
   {
     id: 42,
     title: "Ken's Foods Essentials 1 Gallon Creamy Caesar Dressing",
     code: 'Item #: 999991425',
-    price: '$16.99'
+    price: '$16.99',
   },
   {
     id: 43,
     title:
       'Endless Summer 30" Square LP Gas Outdoor Fire Pit Table with Wood Grain Top',
     code: 'Item #: 472GAD1450',
-    price: '$279.99'
+    price: '$279.99',
   },
   {
     id: 44,
     title: 'Canola Oil - 35 lb.',
     code: 'Item #: 101CANOLABLK',
-    price: '$18.60'
+    price: '$18.60',
   },
   {
     id: 45,
     title: 'Regal #10 Can Sliced Black Olives - 6/Case',
     code: 'Item #: 107685209',
-    price: '$27.39'
+    price: '$27.39',
   },
   {
     id: 46,
     title:
       'Scotsman UN324A-1 24" Air Cooled Undercounter Nugget Ice Machine - 340 lb.',
     code: 'Item #: 720UN324A1 ',
-    price: '$3,543.'
+    price: '$3,543.',
   },
   {
     id: 47,
     title:
       'Bunn VPR Black 12 Cup Pourover Coffee Brewer with 1 Upper and 1 Lower Warmer - 120V',
     code: 'Item #: 23433215',
-    price: '$271.'
+    price: '$271.',
   },
   {
     id: 48,
     title:
       'Beverage-Air SPED36HC-08-2 36" 2 Drawer Refrigerated Sandwich Prep Table',
     code: 'Item #: 185SED3608H',
-    price: '$1,549.'
+    price: '$1,549.',
   },
   {
     id: 49,
     title:
       'Spaceman 6235AH Soft Serve Ice Cream Machine with Air Pump and 2 Hoppers - 208/230V',
     code: 'Item #: 7156235A',
-    price: '$12,604.'
+    price: '$12,604.',
   },
   {
     id: 50,
     title: 'Continental Laundry Cart, Huskee 275 Black X Frame Folding Cart',
     code: 'Item #: 274275BK ',
-    price: '$128.58'
-  }
+    price: '$128.58',
+  },
 ];
+
+(async function () {
+  const [
+    plane0,
+    plane1,
+    plane2,
+    plane3,
+    plane4,
+    plane5,
+    plane6,
+    plane7,
+    plane8,
+    plane9,
+    plane10,
+    plane11,
+    plane12,
+    plane13,
+    plane14,
+    plane15,
+    plane16,
+    plane17,
+    plane18,
+    plane19,
+    plane20,
+    plane21,
+    plane22,
+    plane23,
+    plane24,
+    plane25,
+    plane26,
+    plane27,
+    plane28,
+    plane29,
+    plane30,
+    plane31,
+    plane32,
+    plane33,
+    plane34,
+    plane35,
+    plane36,
+    plane37,
+    plane38,
+    plane39,
+    plane40,
+    plane41,
+    plane42,
+    plane43,
+    plane44,
+    plane45,
+    plane46,
+    plane47,
+    plane48,
+    plane49,
+    plane50,
+  ] = await Promise.all([
+    Scene.root.findFirst('plane0'),
+    Scene.root.findFirst('plane1'),
+    Scene.root.findFirst('plane2'),
+    Scene.root.findFirst('plane3'),
+    Scene.root.findFirst('plane4'),
+    Scene.root.findFirst('plane5'),
+    Scene.root.findFirst('plane6'),
+    Scene.root.findFirst('plane7'),
+    Scene.root.findFirst('plane8'),
+    Scene.root.findFirst('plane9'),
+    Scene.root.findFirst('plane10'),
+    Scene.root.findFirst('plane11'),
+    Scene.root.findFirst('plane12'),
+    Scene.root.findFirst('plane13'),
+    Scene.root.findFirst('plane14'),
+    Scene.root.findFirst('plane15'),
+    Scene.root.findFirst('plane16'),
+    Scene.root.findFirst('plane17'),
+    Scene.root.findFirst('plane18'),
+    Scene.root.findFirst('plane19'),
+    Scene.root.findFirst('plane20'),
+    Scene.root.findFirst('plane21'),
+    Scene.root.findFirst('plane22'),
+    Scene.root.findFirst('plane23'),
+    Scene.root.findFirst('plane24'),
+    Scene.root.findFirst('plane25'),
+    Scene.root.findFirst('plane26'),
+    Scene.root.findFirst('plane27'),
+    Scene.root.findFirst('plane28'),
+    Scene.root.findFirst('plane29'),
+    Scene.root.findFirst('plane30'),
+    Scene.root.findFirst('plane31'),
+    Scene.root.findFirst('plane32'),
+    Scene.root.findFirst('plane33'),
+    Scene.root.findFirst('plane34'),
+    Scene.root.findFirst('plane35'),
+    Scene.root.findFirst('plane36'),
+    Scene.root.findFirst('plane37'),
+    Scene.root.findFirst('plane38'),
+    Scene.root.findFirst('plane39'),
+    Scene.root.findFirst('plane40'),
+    Scene.root.findFirst('plane41'),
+    Scene.root.findFirst('plane42'),
+    Scene.root.findFirst('plane43'),
+    Scene.root.findFirst('plane44'),
+    Scene.root.findFirst('plane45'),
+    Scene.root.findFirst('plane46'),
+    Scene.root.findFirst('plane47'),
+    Scene.root.findFirst('plane48'),
+    Scene.root.findFirst('plane49'),
+    Scene.root.findFirst('plane50'),
+  ]);
+
+  const [
+    text0,
+    text1,
+    text2,
+    text3,
+    text4,
+    text5,
+    text6,
+    text7,
+    text8,
+    text9,
+    text10,
+    text11,
+    text12,
+    text13,
+    text14,
+    text15,
+    text16,
+    text17,
+    text18,
+    text19,
+    text20,
+    text21,
+    text22,
+    text23,
+    text24,
+    text25,
+    text26,
+    text27,
+    text28,
+    text29,
+    text30,
+    text31,
+    text32,
+    text33,
+    text34,
+    text35,
+    text36,
+    text37,
+    text38,
+    text39,
+    text40,
+    text41,
+    text42,
+    text43,
+    text44,
+    text45,
+    text46,
+    text47,
+    text48,
+    text49,
+    text50,
+  ] = await Promise.all([
+    Scene.root.findFirst('text0'),
+    Scene.root.findFirst('text1'),
+    Scene.root.findFirst('text2'),
+    Scene.root.findFirst('text3'),
+    Scene.root.findFirst('text4'),
+    Scene.root.findFirst('text5'),
+    Scene.root.findFirst('text6'),
+    Scene.root.findFirst('text7'),
+    Scene.root.findFirst('text8'),
+    Scene.root.findFirst('text9'),
+    Scene.root.findFirst('text10'),
+    Scene.root.findFirst('text11'),
+    Scene.root.findFirst('text12'),
+    Scene.root.findFirst('text13'),
+    Scene.root.findFirst('text14'),
+    Scene.root.findFirst('text15'),
+    Scene.root.findFirst('text16'),
+    Scene.root.findFirst('text17'),
+    Scene.root.findFirst('text18'),
+    Scene.root.findFirst('text19'),
+    Scene.root.findFirst('text20'),
+    Scene.root.findFirst('text21'),
+    Scene.root.findFirst('text22'),
+    Scene.root.findFirst('text23'),
+    Scene.root.findFirst('text24'),
+    Scene.root.findFirst('text25'),
+    Scene.root.findFirst('text26'),
+    Scene.root.findFirst('text27'),
+    Scene.root.findFirst('text28'),
+    Scene.root.findFirst('text29'),
+    Scene.root.findFirst('text30'),
+    Scene.root.findFirst('text31'),
+    Scene.root.findFirst('text32'),
+    Scene.root.findFirst('text33'),
+    Scene.root.findFirst('text34'),
+    Scene.root.findFirst('text35'),
+    Scene.root.findFirst('text36'),
+    Scene.root.findFirst('text37'),
+    Scene.root.findFirst('text38'),
+    Scene.root.findFirst('text39'),
+    Scene.root.findFirst('text40'),
+    Scene.root.findFirst('text41'),
+    Scene.root.findFirst('text42'),
+    Scene.root.findFirst('text43'),
+    Scene.root.findFirst('text44'),
+    Scene.root.findFirst('text45'),
+    Scene.root.findFirst('text46'),
+    Scene.root.findFirst('text47'),
+    Scene.root.findFirst('text48'),
+    Scene.root.findFirst('text49'),
+    Scene.root.findFirst('text50'),
+  ]);
+
+  catalog[0].plane = plane0;
+  catalog[1].plane = plane1;
+  catalog[2].plane = plane2;
+  catalog[3].plane = plane3;
+  catalog[4].plane = plane4;
+  catalog[5].plane = plane5;
+  catalog[6].plane = plane6;
+  catalog[7].plane = plane7;
+  catalog[8].plane = plane8;
+  catalog[9].plane = plane9;
+  catalog[10].plane = plane10;
+  catalog[11].plane = plane11;
+  catalog[12].plane = plane12;
+  catalog[13].plane = plane13;
+  catalog[14].plane = plane14;
+  catalog[15].plane = plane15;
+  catalog[16].plane = plane16;
+  catalog[17].plane = plane17;
+  catalog[18].plane = plane18;
+  catalog[19].plane = plane19;
+  catalog[20].plane = plane20;
+  catalog[21].plane = plane21;
+  catalog[22].plane = plane22;
+  catalog[23].plane = plane23;
+  catalog[24].plane = plane24;
+  catalog[25].plane = plane25;
+  catalog[26].plane = plane26;
+  catalog[27].plane = plane27;
+  catalog[28].plane = plane28;
+  catalog[29].plane = plane29;
+  catalog[30].plane = plane30;
+  catalog[31].plane = plane31;
+  catalog[32].plane = plane32;
+  catalog[33].plane = plane33;
+  catalog[34].plane = plane34;
+  catalog[35].plane = plane35;
+  catalog[36].plane = plane36;
+  catalog[37].plane = plane37;
+  catalog[38].plane = plane38;
+  catalog[39].plane = plane39;
+  catalog[40].plane = plane40;
+  catalog[41].plane = plane41;
+  catalog[42].plane = plane42;
+  catalog[43].plane = plane43;
+  catalog[44].plane = plane44;
+  catalog[45].plane = plane45;
+  catalog[46].plane = plane46;
+  catalog[47].plane = plane47;
+  catalog[48].plane = plane48;
+  catalog[49].plane = plane49;
+  catalog[50].plane = plane50;
+
+  catalog[0].text = text0;
+  catalog[1].text = text1;
+  catalog[2].text = text2;
+  catalog[3].text = text3;
+  catalog[4].text = text4;
+  catalog[5].text = text5;
+  catalog[6].text = text6;
+  catalog[7].text = text7;
+  catalog[8].text = text8;
+  catalog[9].text = text9;
+  catalog[10].text = text10;
+  catalog[11].text = text11;
+  catalog[12].text = text12;
+  catalog[13].text = text13;
+  catalog[14].text = text14;
+  catalog[15].text = text15;
+  catalog[16].text = text16;
+  catalog[17].text = text17;
+  catalog[18].text = text18;
+  catalog[19].text = text19;
+  catalog[20].text = text20;
+  catalog[21].text = text21;
+  catalog[22].text = text22;
+  catalog[23].text = text23;
+  catalog[24].text = text24;
+  catalog[25].text = text25;
+  catalog[26].text = text26;
+  catalog[27].text = text27;
+  catalog[28].text = text28;
+  catalog[29].text = text29;
+  catalog[30].text = text30;
+  catalog[31].text = text31;
+  catalog[32].text = text32;
+  catalog[33].text = text33;
+  catalog[34].text = text34;
+  catalog[35].text = text35;
+  catalog[36].text = text36;
+  catalog[37].text = text37;
+  catalog[38].text = text38;
+  catalog[39].text = text39;
+  catalog[40].text = text40;
+  catalog[41].text = text41;
+  catalog[42].text = text42;
+  catalog[43].text = text43;
+  catalog[44].text = text44;
+  catalog[45].text = text45;
+  catalog[46].text = text46;
+  catalog[47].text = text47;
+  catalog[48].text = text48;
+  catalog[49].text = text49;
+  catalog[50].text = text50;
+
+  for (let i = 0; i < catalog.length; i++) {
+    catalog[i].plane.hidden = true;
+    catalog[i].text.hidden = true;
+  }
+})();
--- Webstaurant/scripts/main.js
+++ Webstaurant/scripts/main.js
@@ -10,83 +10,73 @@ const Audio = require('Audio');
 import { catalog } from './catalog.js';
 import { shuffle } from './util.js';
 
-// Add planes to catalog
-const planeLogger = catalog => {
-  for (let i = 0; i < catalog.length; i++) {
-    catalog[i].plane = Scene.root.find(`plane${i}`);
-    catalog[i].plane.hidden = true;
-    catalog[i].text = Scene.root.find(`text${i}`);
-    catalog[i].text.hidden = true;
-  }
-};
+(async function () {
+  const shuffledCatalog = await shuffle(catalog);
 
-planeLogger(catalog);
-const shuffledCatalog = shuffle(catalog);
+  const [textContainer, titleCard, playbackController] = await Promise.all([
+    Scene.root.findFirst('text-container'),
+    Scene.root.findFirst('title-card'),
+    Audio.getAudioPlaybackController('audioPlaybackController0'),
+  ]);
 
-// text-container is visible, but still size (0,0)
-const textContainer = Scene.root.find('text-container');
-textContainer.hidden = false;
+  // text-container is visible, but still size (0,0)
+  textContainer.hidden = false;
+  titleCard.hidden = false;
 
-const titleCard = Scene.root.find('title-card');
-titleCard.hidden = false;
+  // EventSource to trigger when user starts recording
+  const recordingEvent = CameraInfo.isRecordingVideo.onOn();
 
-// EventSource to trigger when user starts recording
-const recordingEvent = CameraInfo.isRecordingVideo.onOn();
+  recordingEvent.subscribe(() => {
+    const randomRunner = () => {
+      // This function is the meat of the program
+      titleCard.hidden = true;
+      let playing = Reactive.val(true);
+      let lastRandom = 0;
 
-const playbackController = Audio.getPlaybackController(
-  'audioPlaybackController0'
-);
+      const shuffleRunner = () => {
+        // Starts ticking sound
+        playbackController.setLooping(playing.pinLastValue());
+        playbackController.setPlaying(playing.pinLastValue());
 
-recordingEvent.subscribe(() => {
-  const randomRunner = () => {
-    // This function is the meat of the program
-    titleCard.hidden = true;
-    let playing = Reactive.val(true);
-    let lastRandom = 0;
+        const shuffleImages = () => {
+          // Shuffles through the images
+          shuffledCatalog[lastRandom].plane.hidden = true;
+          lastRandom = lastRandom + 1;
+          if (lastRandom >= shuffledCatalog.length - 1) lastRandom = 0;
+          shuffledCatalog[lastRandom].plane.hidden = false;
+        };
 
-    const shuffleRunner = () => {
-      // Starts ticking sound
-      playbackController.setLooping(playing.pinLastValue());
-      playbackController.setPlaying(playing.pinLastValue());
-
-      const shuffleImages = () => {
-        // Shuffles through the images
-        shuffledCatalog[lastRandom].plane.hidden = true;
-        lastRandom = lastRandom + 1;
-        if (lastRandom >= shuffledCatalog.length - 1) lastRandom = 0;
-        shuffledCatalog[lastRandom].plane.hidden = false;
+        // Swaps image every 80ms, stops after 5s
+        const randomItems = Time.setInterval(shuffleImages, 80);
+        Time.setTimeout(() => Time.clearInterval(randomItems), 5000);
       };
 
-      // Swaps image every 80ms, stops after 5s
-      const randomItems = Time.setInterval(shuffleImages, 80);
-      Time.setTimeout(() => Time.clearInterval(randomItems), 5000);
-    };
+      const showInfo = () => {
+        // Stops ticking sound
+        playing = Reactive.val(false);
+        playbackController.setPlaying(playing.pinLastValue());
 
-    const showInfo = () => {
-      // Stops ticking sound
-      playing = Reactive.val(false);
-      playbackController.setPlaying(playing.pinLastValue());
-
-      // Expands text information
-      const timeDriverParameters = {
-        durationMilliseconds: 500,
-        loopCount: 1,
-        mirror: false
+        // Expands text information
+        const timeDriverParameters = {
+          durationMilliseconds: 500,
+          loopCount: 1,
+          mirror: false,
+        };
+        const timeDriver = Animation.timeDriver(timeDriverParameters);
+        const sampler = Animation.samplers.linear(0, 0.6);
+        const translationAnimation = Animation.animate(timeDriver, sampler);
+        const xScale = Reactive.val(4);
+        shuffledCatalog[lastRandom].text.hidden = false;
+        textContainer.transform.scaleY = translationAnimation;
+        textContainer.transform.scaleX = translationAnimation.mul(xScale);
+        timeDriver.start();
       };
-      const timeDriver = Animation.timeDriver(timeDriverParameters);
-      const sampler = Animation.samplers.linear(0, 0.6);
-      const translationAnimation = Animation.animate(timeDriver, sampler);
-      const xScale = Reactive.val(4);
-      shuffledCatalog[lastRandom].text.hidden = false;
-      textContainer.transform.scaleY = translationAnimation;
-      textContainer.transform.scaleX = translationAnimation.mul(xScale);
-      timeDriver.start();
+
+      shuffleRunner();
+      Time.setTimeout(showInfo, 5000);
     };
 
-    shuffleRunner();
-    Time.setTimeout(showInfo, 5000);
-  };
-
-  // 1 second after recording starts, trigger the program
-  Time.setTimeout(randomRunner, 2000);
-});
+    // 1 second after recording starts, trigger the program
+    Time.setTimeout(randomRunner, 2000);
+  });
+})();
