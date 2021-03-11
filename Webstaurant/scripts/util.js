a026c7800f475a7e92ab02570ce5aede4d3d1787
Max David <me@maxdavid.com>
2021-03-11 11:23:48 -0800

    Fix bug with Spark's Random lib

--- Webstaurant/scripts/util.js
+++ Webstaurant/scripts/util.js
@@ -1,10 +1,17 @@
-export const shuffle = array => {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+const Random = require('Random');
+
+export const shuffle = (inputArray) => {
+  // Deep clone
+  const clone = (items) =>
+    items.map((item) => (Array.isArray(item) ? clone(item) : item));
+
+  let array = clone(inputArray);
+  let currentIndex = array.length;
+  let temporaryValue = 0;
+  let randomIndex = 0;
 
   while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
+    randomIndex = Math.floor(Random.random() * currentIndex);
     currentIndex -= 1;
 
     temporaryValue = array[currentIndex];
